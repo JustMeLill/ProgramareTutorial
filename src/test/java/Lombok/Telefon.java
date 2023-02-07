23ab8b3cb0727f826c08eeabd499cc0f4019a1a4
JustMeLill <dutescu_lilly@yahoo.com>
2023-02-07 19:08:37 +0200

    Lombok

---
+++ src/test/java/Lombok/Telefon.java
@@ -0,0 +1,18 @@
+package Lombok;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Telefon {
+
+    private String Brand;
+    private String Model;
+    private Integer AnFabricatie;
+    private String Material;
+    private String Camera;
+
+}
